a23076da748592dbef9d6b2969c3c18c612262a1
adanehiasher <adanehiasher@gmail.com>
2023-11-11 01:48:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+let toastBox = document.getElementById("toastBox");
+let successMsg = "Successfully submitted";
+let errorMsg = "Please fix the error submitted";
+let invalidMsg = "Invalid input, check again";
+
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.className = "toast";
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+    
+    if(msg.includes('error')){
+        toast.classList.add('error')
+    }
+
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid')
+    }
+
+
+    setTimeout(()=>{
+        toast.remove()
+    }, 3000)
+
+
+    console.log(toast)
+}
